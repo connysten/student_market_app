cf59f1b905ccfcbafcdb5e09f9567ebb57bdcc5c
Dennis Tagesson <dennistagesson1@gmail.com>
2019-12-18 17:07:35 +0100

    stuff

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -3,42 +3,12 @@
 //
 
 #import "GeneratedPluginRegistrant.h"
-
-#if __has_include(<barcode_scan/BarcodeScanPlugin.h>)
 #import <barcode_scan/BarcodeScanPlugin.h>
-#else
-@import barcode_scan;
-#endif
-
-#if __has_include(<cloud_firestore/CloudFirestorePlugin.h>)
 #import <cloud_firestore/CloudFirestorePlugin.h>
-#else
-@import cloud_firestore;
-#endif
-
-#if __has_include(<firebase_auth/FirebaseAuthPlugin.h>)
 #import <firebase_auth/FirebaseAuthPlugin.h>
-#else
-@import firebase_auth;
-#endif
-
-#if __has_include(<firebase_core/FirebaseCorePlugin.h>)
 #import <firebase_core/FirebaseCorePlugin.h>
-#else
-@import firebase_core;
-#endif
-
-#if __has_include(<google_sign_in/GoogleSignInPlugin.h>)
 #import <google_sign_in/GoogleSignInPlugin.h>
-#else
-@import google_sign_in;
-#endif
-
-#if __has_include(<image_picker/ImagePickerPlugin.h>)
 #import <image_picker/ImagePickerPlugin.h>
-#else
-@import image_picker;
-#endif
 
 @implementation GeneratedPluginRegistrant
 
