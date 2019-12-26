c33f0d4ea4ad417da4fdda870c81c360413a29af
Dennis Tagesson <dennistagesson1@gmail.com>
2019-12-26 22:43:33 +0100

    Sell.dart now uploads image to database

--- android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -5,6 +5,7 @@ import com.apptreesoftware.barcodescan.BarcodeScanPlugin;
 import io.flutter.plugins.firebase.cloudfirestore.CloudFirestorePlugin;
 import io.flutter.plugins.firebaseauth.FirebaseAuthPlugin;
 import io.flutter.plugins.firebase.core.FirebaseCorePlugin;
+import io.flutter.plugins.firebase.storage.FirebaseStoragePlugin;
 import io.flutter.plugins.googlesignin.GoogleSignInPlugin;
 import io.flutter.plugins.imagepicker.ImagePickerPlugin;
 
@@ -20,6 +21,7 @@ public final class GeneratedPluginRegistrant {
     CloudFirestorePlugin.registerWith(registry.registrarFor("io.flutter.plugins.firebase.cloudfirestore.CloudFirestorePlugin"));
     FirebaseAuthPlugin.registerWith(registry.registrarFor("io.flutter.plugins.firebaseauth.FirebaseAuthPlugin"));
     FirebaseCorePlugin.registerWith(registry.registrarFor("io.flutter.plugins.firebase.core.FirebaseCorePlugin"));
+    FirebaseStoragePlugin.registerWith(registry.registrarFor("io.flutter.plugins.firebase.storage.FirebaseStoragePlugin"));
     GoogleSignInPlugin.registerWith(registry.registrarFor("io.flutter.plugins.googlesignin.GoogleSignInPlugin"));
     ImagePickerPlugin.registerWith(registry.registrarFor("io.flutter.plugins.imagepicker.ImagePickerPlugin"));
   }
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,11 +1,16 @@
+import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:uuid/uuid.dart';
 
 class DatabaseService {
   //collection reference
   final CollectionReference addCollection = Firestore.instance.collection('Annons');
-  int docId = 0;
 
-  Future createAdd(String title, String author, String language, int isbn, double price, String condition, String description, String userid) async {
+  Future createAdd(String title, String author, String language, int isbn, double price, String condition, String description, String userid, File image) async {
+    var tempUrl = await addImage(image, title);
+    var imageUrl = tempUrl.toString();
     return await addCollection.add({
       'title': title,
       'author': author,
@@ -15,7 +20,21 @@ class DatabaseService {
       'condition': condition,
       'description': description,
       'userid': userid,
+      'imageUrl': imageUrl,
     });
+  }
+
+  Future<String> addImage(File image, String title) async {
+    var uploadedFileUrl;
+    var uuid = Uuid().v1();
+    StorageReference ref = FirebaseStorage.instance.ref().child('books/$uuid');
+    /*StorageUploadTask uploadTask = ref.putFile(image);
+    ref.getDownloadURL().then((fileURL) {
+      uploadedFileUrl = fileURL;
+    });
+    return uploadedFileUrl;*/
+    StorageUploadTask uploadTask = ref.putFile(image);
+    return await(await uploadTask.onComplete).ref.getDownloadURL();
   }
 
 /*Stream<Add> get add {
